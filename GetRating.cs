a1f1fd35e0066175c8501df66a513b3c26207b09
Sravani Seethi <sseethi@microsoft.com>
2022-03-16 16:52:32 +0000

    Added 8 files to /

---
+++ GetRating.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using ServerlessTeam9FunctionApp.Models;
+
+namespace ServerlessTeam9FunctionApp
+{
+    public static class GetRating
+    {
+        [FunctionName("GetRating")]
+        public static IActionResult Run(
+       [HttpTrigger(AuthorizationLevel.Function, "get", Route = "rating/{ratingId}")] HttpRequest req,
+        [CosmosDB("%databaseName%", "%collectionName%", ConnectionStringSetting = "_ConnectionStringSetting",
+        SqlQuery = "Select * from ratings r where r.id = {ratingId}")]IEnumerable<RatingModel> rating,
+         ILogger log)
+        {
+            if (rating == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(rating);
+        }
+    }
+}
---
+++ GetRatings.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using ServerlessTeam9FunctionApp.Models;
+
+namespace ServerlessTeam9FunctionApp
+{
+    public static class GetRatings
+    {
+        [FunctionName("GetRatings")]
+        public static IActionResult Run(
+       [HttpTrigger(AuthorizationLevel.Function, "get", Route = "ratingsByUser/{userId}")] HttpRequest req,
+        [CosmosDB("%databaseName%", "%collectionName%", ConnectionStringSetting = "_ConnectionStringSetting",
+        SqlQuery = "Select * from ratings r where r.userId = {userId}")] IEnumerable<RatingModel> ratings,
+         ILogger log)
+        {
+            if (ratings == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new OkObjectResult(ratings);
+        }
+    }
+}
