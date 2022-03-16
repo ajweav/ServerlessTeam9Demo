fa5346018c248d3e28bee0f713dbb0836e5fd6d8
Sravani Seethi <sseethi@microsoft.com>
2022-03-16 16:53:15 +0000

    Added RatingModel.cs to /

---
+++ RatingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerlessTeam9FunctionApp.Models
+{
+    public class RatingModel
+    {
+        public string userId { get; set; }
+        public string productId { get; set; }
+        public string locationName { get; set; }
+        public int rating { get; set; }
+        public string userNotes { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+
+}
